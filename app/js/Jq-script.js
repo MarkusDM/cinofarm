f16895e1eee04b567888b3e7fc45113d93c57ab9
Dmitry <mazarini2002@yandex.ru>
2023-09-08 02:48:45 +0500

    news-page

--- app/js/Jq-script.js
+++ app/js/Jq-script.js
@@ -1,17 +1,17 @@
 'use strict';
 
  
-$('.header__phone').hover(function () {
+$('.header__data-item').hover(function () {
        
     clearTimeout($.data(this,'timer'));
 
-    $('.header__phone-lists',this).stop(true,true).slideDown(400);
+    $('.header__data-block',this).stop(true,true).slideDown(400);
 
  }, function () {
 
    $.data(this,'timer', setTimeout($.proxy(function() {
 
-     $('.header__phone-lists',this).stop(true,true).slideUp(400);
+     $('.header__data-block',this).stop(true,true).slideUp(400);
 
    }, this), 100));
 
